a191118b0f84716555e6a6f80198638c44404a58
Thalles Passos <contato@thalles.me>
2025-05-23 12:21:38 -0300

    commit remaining things

--- examples/privy-nextjs-notus-api/src/components/providers.tsx
+++ examples/privy-nextjs-notus-api/src/components/providers.tsx
@@ -15,14 +15,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 			<PrivyProvider
 				appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
 				clientId={env.NEXT_PUBLIC_PRIVY_CLIENT_ID}
-				config={{
-					// Create embedded wallets for users who don't have a wallet
-					embeddedWallets: {
-						ethereum: {
-							createOnLogin: "all-users",
-						},
-					},
-				}}
 			>
 				{children}
 			</PrivyProvider>
