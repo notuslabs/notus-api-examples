eb2219d06938accb36561d42e2fb6baf3bcaf954
Thalles Passos <contato@thalles.me>
2025-06-30 16:31:28 -0300

    add examples

--- examples/kyc-react-native/app/index.tsx
+++ examples/kyc-react-native/app/index.tsx
@@ -1,18 +1,66 @@
-import { Pressable, Text, View } from "react-native";
-import {} from "@notus-api/react-native-sdk";
+import { Liveness } from "@notus-api/expo-sdk";
+import { useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 export default function Index() {
-	return (
+	const [showLiveness, setShowLiveness] = useState(false);
+	const [message, setMessage] = useState<string | null>(null);
+	const [sessionId, setSessionId] = useState<string | undefined>(undefined);
+
+	return showLiveness && sessionId ? (
+		<Liveness
+			sessionId={sessionId}
+			onSuccess={() => {
+				setShowLiveness(false);
+				setMessage("Liveness completed successfully");
+			}}
+			onError={(error) => {
+				setMessage(error.message);
+			}}
+		/>
+	) : (
 		<View
 			style={{
-				flex: 1,
-				justifyContent: "center",
 				alignItems: "center",
+				justifyContent: "center",
+				height: "100%",
+				gap: 10,
+				padding: 10,
 			}}
 		>
-			<Pressable style={{ backgroundColor: "red" }} onPress={() => {}}>
-				<Text>Hello</Text>
+			<View
+				style={{
+					flexDirection: "column",
+					gap: 10,
+					width: "100%",
+				}}
+			>
+				<Text>Input your KYC session ID:</Text>
+				<TextInput
+					placeholder="Session ID"
+					value={sessionId}
+					onChangeText={setSessionId}
+					style={{
+						borderWidth: 1,
+						borderColor: "black",
+						backgroundColor: "white",
+						padding: 10,
+					}}
+				/>
+			</View>
+			<Pressable
+				onPress={() => {
+					setShowLiveness(true);
+				}}
+				style={{
+					backgroundColor: "white",
+					padding: 10,
+					borderRadius: 5,
+				}}
+			>
+				<Text>Start Liveness</Text>
 			</Pressable>
+			{message && <Text>{message}</Text>}
 		</View>
 	);
 }
--- examples/kyc-react-native/metro.config.js
+++ examples/kyc-react-native/metro.config.js
@@ -1,5 +1,44 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
+const path = require("node:path");
 
-module.exports = config;
+const myExtraModuleDir = path.resolve(
+	__dirname,
+	"../../../notus-api/packages/expo-sdk",
+);
+const extraNodeModules = {
+	"@notus-api/expo-sdk": myExtraModuleDir,
+};
+const watchFolders = [
+	// Include extra module dir to work around Metro's bug where resolver.extraNodeModules
+	// does not work without corresponding watchFolders, see also
+	// https://github.com/facebook/metro/issues/834
+	myExtraModuleDir,
+];
+
+const config = {
+	watchFolders: watchFolders,
+	resolver: {
+		extraNodeModules: new Proxy(extraNodeModules, {
+			get: (target, name) =>
+				// redirects dependencies referenced from myExtraModule/ to local node_modules
+				name in target
+					? target[name]
+					: path.join(process.cwd(), `node_modules/${name}`),
+		}),
+		// unstable_enableSymlinks: true,  // defaults to true since Metro v0.79.0
+	},
+	resetCache: true, // https://metrobundler.dev/docs/configuration/#resetcache
+};
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+	...defaultConfig,
+	...config,
+	resolver: {
+		...defaultConfig.resolver,
+		...config.resolver,
+	},
+	watchFolders: [...(defaultConfig.watchFolders || []), ...config.watchFolders],
+};
